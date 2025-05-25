7bb066ccdf8fa504032dc8c42b0cbef0fc330b1c
WorldWorldWorld <tomo_0817@i.softbank.jp>
2025-05-25 20:41:29 +0900

    サーバー更新

--- server.js
+++ server.js
@@ -7,16 +7,31 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// メッセージリスト
+const wsList = [];
+
 wss.on('connection', (ws) => {
   console.log('クライアント接続');
 
   ws.on('message', (message) => {
-    console.log('受信:', message);
-    ws.send(`サーバーからの返信: ${message}`);
+    console.log('受信したメッセージ:', Stirng(message));
+    ws.send(`サーバーから返信: ${message}`);
+
+    // ▽サンプル ------------------------------------------------
+    // メッセージリストに追加
+    wsList.push(message);
+    // 送られていないメッセージを送る
+    for (const wsMessage of wsList) {
+      if (message != wsMessage) {
+        socket.send('未送信メッセージ：'+ wsMessage);
+      }
+    }
+    //------------------------------------------------------------
+
   });
 
   ws.on('close', () => {
-    console.log('切断されました');
+    console.log('クライアントが切断されました');
   });
 });
 
